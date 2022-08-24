9db4591377eedbbde01fd35d3d091e6b05cb09a9
Lucas Silva <lucasfic2017@gmail.com>
2022-08-24 17:48:46 -0300

    adiciona o requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -24,8 +24,9 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let total = wins * 3 + ties;
+  return total;
 }
 
 // Desafio 6
